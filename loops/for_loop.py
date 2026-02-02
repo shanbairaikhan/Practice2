9aeb00e731c447cf42f72d4a0dfff7632a4f8677
shanbairaikhan <raikhanshanbai-8@gmail.com>
2026-02-02 17:18:52 +0500

    for commit1

---
+++ loops/for_loop.py
@@ -0,0 +1,25 @@
+#1
+for i in range(5):
+    print(i)
+#2
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+#3
+for char in "Python":
+    print(char)
+#4
+total = 0
+for i in range(1, 6):
+    total += i
+print("Sum:", total)
+#5
+for i in range(1, 6):
+    print(i, "squared is", i**2)
+#6
+for i in range(1, 4):
+    for j in range(1, 4):
+        print(i, "*", j, "=", i*j)
+#7
+for i in range(0, 10, 2):
+    print(i)
