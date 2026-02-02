2d4ea29bae2e2a3a80f465d701209d04bf4ae247
shanbairaikhan <raikhanshanbai-8@gmail.com>
2026-02-02 17:30:51 +0500

    while loop1

---
+++ loops/while_loop.py
@@ -0,0 +1,30 @@
+#1
+i = 5
+while i > 0:
+    print(i)
+    i -= 1
+#2
+total = 0
+i = 1
+while total < 10:
+    total += i
+    print(total)
+    i += 1
+#3
+fruits = ["apple", "banana", "cherry"]
+i = 0
+while i < len(fruits):
+    print(fruits[i])
+    i += 1
+#4
+x = 10
+while x > 0:
+    print(x)
+    x -= 2
+#5
+i = 1
+total = 0
+while total < 15:
+    total += i
+    print("Adding", i, "Total:", total)
+    i += 1
